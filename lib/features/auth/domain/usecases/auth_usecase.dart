38069fa17b47a31c2bdcb5aa87d8c8b6e972766c
TheBinKingVII <bintoronob@gmail.com>
2026-05-05 19:42:04 +0700

    update auth usecase

--- lib/features/auth/domain/usecases/auth_usecase.dart
+++ lib/features/auth/domain/usecases/auth_usecase.dart
@@ -1,1 +1,39 @@
-class AuthUsecase {}
+import '../entities/auth_entity.dart';
+import '../repositories/auth_repository.dart';
+
+class AuthUsecase {
+  AuthUsecase(this._repository);
+
+  final AuthRepository _repository;
+
+  Future<AuthEntity> login({
+    required String email,
+    required String password,
+  }) {
+    return _repository.login(email: email, password: password);
+  }
+
+  Future<AuthEntity> register({
+    required String fullName,
+    required String email,
+    required String password,
+  }) {
+    return _repository.register(
+      fullName: fullName,
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<AuthEntity> loginWithBiometric() {
+    return _repository.loginWithBiometric();
+  }
+
+  Future<bool> canShowBiometricLogin() {
+    return _repository.canShowBiometricLogin();
+  }
+
+  Future<void> logout() {
+    return _repository.logout();
+  }
+}
